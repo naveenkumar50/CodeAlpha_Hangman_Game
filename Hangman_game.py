ebab0a29d31cfe2abd3962da3ae44c807ffa55c0
NAVEENKUMAR A <anknaveen10@gmail.com>
2025-05-17 14:25:28 +0530

    Add files via upload

---
+++ Hangman_game.py
@@ -0,0 +1,40 @@
+import random
+
+words = ["apple", "banana", "cherry", "orange", "grapezxd"]
+
+while True:
+    word = random.choice(words)
+    guessed = ""
+    tries = 6
+
+    print("\nNew Game Started!")
+
+    while tries > 0:
+        fail = 0
+        for letter in word:
+            if letter in guessed:
+                print(letter, end=" ")
+            else:
+                print("_", end=" ")
+                fail += 1
+        print()
+
+        if fail == 0:
+            print("You win! The word was:", word)
+            break
+
+        guess = input("Guess a letter: ")
+        if guess == "stop":
+            print("Thanks for playing!")
+            exit()
+        if guess in guessed:
+            print("Already guessed.")
+            continue
+
+        guessed += guess
+
+        if guess not in word:
+            tries -= 1
+            print("Wrong! Tries left:", tries)
+            if tries == 0:
+                print("Game over! The word was:", word)
